cc6eb751737c523c7b397360043e5b81b747d052
davila29v <davila29v@hotmail.com>
2021-04-09 03:31:16 +0000

    avance

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask , jsonify
+app = Flask(__name__)
+todos = [
+    { "label": "My first task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+   
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos',methods)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
